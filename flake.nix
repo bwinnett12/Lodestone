a4d904b7ac5408c71589baf601df6bc5a0f0debe
William Winnett <bwinnett12@gmail.com>
2025-07-12 00:49:08 -0800

    Added flakes and initial home manager structure

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Treehouse: Loom - Surface Book 2 on NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05"; # IMPORTANT: Match your NixOS version
+    # Home Manager; home manager; home-manager; HomeManager 
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05"; # IMPORTANT: Match your NixOS version
+      inputs.nixpkgs.follows = "nixpkgs"; 
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.Loom = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";  
+      modules = [
+        ./configuration.nix # Base configuration
+        
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.yourUsername = import ./home.nix; # Create this file soon
+        }
+      ];
+      specialArgs = { inherit inputs; }; # Pass inputs to modules if needed
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  # Set your state version for Home Manager
+  home.stateVersion = "24.05"; # Match your NixOS release
+
+  home.packages = with pkgs; [
+    htop
+    neofetch
+  ];
+
+  programs.bash.enable = true;
+  # programs.zsh.enable = true; 
+
+  # Other user-level configurations will go here
+}

4f31f0a4978d86d30be2968a418436b82427e06b
William Winnett <bwinnett12@gmail.com>
2025-07-20 00:32:08 -0800

    Changed surface package location

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       modules = [
         ./hosts/loom/configuration.nix
         ./hosts/loom/hardware-configuration.nix
-        nixos-hardware.nixosModules.microsoft-surface.common
+        nixos-hardware.nixosModules.surface.common
       ];
 
       # You can pass additional arguments to your modules here if needed.

f3cb5ce332bbea85f5ffb6896bd806ccdbeb87fe
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-10-08 13:44:31 -0800

    Updated Config files. Orchid -> btrfs.... Typo

--- hosts/loom/configuration.nix
+++ hosts/loom/configuration.nix
@@ -174,21 +174,6 @@
 
 
 
-
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #  font = "Lat2-Terminus16";
-  #  keyMap = "us";
-  #  useXkbConfig = true; # use xkb.options in tty.
-  #};
-
-
-
-
-
-
   ## Systemd configuration for disabling auto sleep
   #systemd.targets = {
   #  sleep.enable = false;

c808aef2a2b74b0e62c44ce15e5a80675e796680
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-10-10 09:17:51 -0800

    Changed home-manager version

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
 
     ## ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ ##
     ## Home manager
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
 

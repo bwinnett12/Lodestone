18a11763570f5151c769b7717bd21b2442b82794
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-10-21 20:54:26 -0800

    Fixed ports

--- hosts/island/configuration.nix
+++ hosts/island/configuration.nix
@@ -319,7 +319,7 @@
     # Configuration for the internal Komga Spring Boot application
     settings = {
       server.port = 2104;
-      address = "127.0.0.1";
+      address = "0.0.0.0";
     };
   };
 

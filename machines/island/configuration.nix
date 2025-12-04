549a5d9a3ff18863c8d995924cad587df3e3e2e1
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-12-03 22:37:35 -0900

    Home manager 8 lol

--- machines/island/configuration.nix
+++ machines/island/configuration.nix
@@ -251,39 +251,39 @@
 
 
 
-#  ## ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ ##
-#  ## Local AI by docker-compose
-#
-#
-#
-#  systemd.services.localai-docker-compose = {
-#
-#    ### LocalAI
-#    description = "LocalAI via Docker Compose";
-#
-#
-#    # Wait for network and your storage mount to be ready
-#    # after = [ "network.target" "docker.service" "storage-Orchid.mount" ];
-#    
-#    requires = [ "docker.service" ];
-#    wantedBy = [ "multi-user.target" ];
-#
-#
-#    serviceConfig = {
-#      # Replace the path with wherever you put your docker-compose.yml
-#      ExecStart = "${pkgs.docker-compose}/bin/docker-compose -f /etc/localai/docker-compose.yml up"; 
-#      ExecStop = "${pkgs.docker-compose}/bin/docker-compose -f /etc/localai/docker-compose.yml down";
-#
-#      ## Currently using root
-#      ## #todo - Switch to localai or shortstack user
-#      ## Currently root
-#      User = "root";
-#      # Set the working directory to the directory of the compose file
-#      WorkingDirectory = "/etc/localai"; 
-#      Restart = "on-failure";
-#      RestartSec = "5s";
-#  };
-#};
+  ## ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ ##
+  ## Local AI by docker-compose
+
+
+
+  systemd.services.localai-docker-compose = {
+
+    ### LocalAI
+    description = "LocalAI via Docker Compose";
+
+
+    # Wait for network and your storage mount to be ready
+    # after = [ "network.target" "docker.service" ];
+    
+    requires = [ "docker.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+
+    serviceConfig = {
+      # Replace the path with wherever you put your docker-compose.yml
+      ExecStart = "${pkgs.docker-compose}/bin/docker-compose -f /etc/localai/docker-compose.yml up"; 
+      ExecStop = "${pkgs.docker-compose}/bin/docker-compose -f /etc/localai/docker-compose.yml down";
+
+      ## Currently using root
+      ## #todo - Switch to localai or shortstack user
+      ## Currently root
+      User = "root";
+      # Set the working directory to the directory of the compose file
+      WorkingDirectory = "/etc/localai"; 
+      Restart = "on-failure";
+      RestartSec = "5s";
+  };
+};
 
 
 

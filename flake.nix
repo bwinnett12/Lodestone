4f6bfe503d43b1a950abd3b32a46f9685b7f41f1
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-12-03 21:23:19 -0900

    Home manager 4 lol

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
 
     homeConfigurations = {
       "tarobutter" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.packages.x86_64-linux;
+        inherit (nixpkgs) pkgs;
         configuration = import ./home.nix // {
           profiles = {
             academic = { enable = true; };

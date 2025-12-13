1b3ea353b696468fa5feabd63755e18402a6eea2
Bell <60406893+bwinnett12@users.noreply.github.com>
2025-12-12 16:36:53 -0900

    Guacamole 15

--- machines/Island/modules/guacamole/default.nix
+++ machines/Island/modules/guacamole/default.nix
@@ -33,7 +33,14 @@
         pkgs.guacamole-client
       ];
     # extraEnvironment = [ "GUACAMOLE_HOME=/etc/guacamole" ];
-    };  
+    };
+
+      ### XRDP Service
+  services.xrdp = {
+    enable = true;  # Enable the XRDP service
+    #defaultWindowManager = "startplasma-x11";  # Change this if you're using a different window manager
+    openFirewall = true;  # This will allow traffic through the firewall for RDP
+  };
 
 	environment.systemPackages = with pkgs; [
 		guacamole-client

933fac0807d7cde7d6c634ca0e45d6635402deff
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-02-21 02:11:54 +0000

    Prometheus alternative 3: Added node to port

--- machines/Island/configuration.nix
+++ machines/Island/configuration.nix
@@ -35,15 +35,21 @@
       8090  ## 8080 - LocalAI
 
 
+      4822  ## 4822 - Guacamole
+      3389  ## 3389 - Guacamole
+
       2104  ## 2104 - Komga
+
+      9000  ## 9000 - Prometheus
 
       2108  ## 2108 - Suwayomi Server
       4567  ## 4567 - Suwayomi Server
 
-      4822  ## 4822 - Guacamole
-      3389  ## 3389 - Guacamole
 
       4500  ## 4500 - u9fs
+
+
+
 
       21115  ## Rustdesk
       21116  ## Rustdesk

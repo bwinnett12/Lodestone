538ad8e9cb4178bd2e64c2ccb878871f2bbe116c
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-02-21 02:08:36 +0000

    Prometheus alternative

--- resources/modules/prometheus/default.nix
+++ resources/modules/prometheus/default.nix
@@ -1,43 +1,18 @@
-#### ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ ##
-#### Prometheus module through docker
-
-{ config, pkgs, inputs, ... }:
-
+{ config, pkgs, ... }:
 {
-  systemd.services.prometheus = {
-
-	### Prometheus
-	description = "Prometheus via Docker Compose";
-
-	# Wait for network and your storage mount to be ready
-	after = [ "network.target" "docker.service" ];
-
-	requires = [ "docker.service" ];
-	wantedBy = [ "multi-user.target" ];
-
-  serviceConfig = {
-    ExecStart = "${pkgs.docker-compose}/bin/docker-compose -f /home/tarobutter/Lodestone/resources/modules/prometheus/prometheus-docker.yml up";
-	ExecStop = "${pkgs.docker-compose}/bin/docker-compose -f /home/tarobutter/Lodestone/resources/modules/prometheus/prometheus-docker.yml down";
-
-	## TODO - Switch to localai or shortstack user
-	User = "root";
-	# Set the working directory to the directory of the compose file
-	WorkingDirectory = "/storage/Orchid/shortstack/prometheus/"; 
-	Restart = "on-failure";
-	RestartSec = "5s";
+  # https://wiki.nixos.org/wiki/Prometheus
+  # https://nixos.org/manual/nixos/stable/#module-services-prometheus-exporters-configuration
+  # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/services/monitoring/prometheus/default.nix
+  services.prometheus = {
+    enable = true;
+    globalConfig.scrape_interval = "10s"; # "1m"
+    scrapeConfigs = [
+      {
+        job_name = "node";
+        static_configs = [{
+          targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+        }];
+      }
+    ];
   };
-
-};
-	# Define a user account. Don't forget to set a password with ‘passwd’
-users.users.prometheus = {
-	isNormalUser = true;
-	extraGroups = [ "wheel" "docker" ];
-	packages = with pkgs; [
-		prometheus
-		tree
-	];
-  };
-      
 }
-
-

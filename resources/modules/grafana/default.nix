2c87cd4d645838f584c0a4511da184dd999198f5
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-02-21 05:29:17 +0000

    Grafana: Added nginx

--- resources/modules/grafana/default.nix
+++ resources/modules/grafana/default.nix
@@ -7,7 +7,7 @@
         http_port = 3000;
         enforce_domain = true;
         enable_gzip = true;
-        domain = "platatoo";
+        domain = "grafana.platatoo";
 
         # Alternatively, if you want to serve Grafana from a subpath:
         # domain = "your.domain";
@@ -17,6 +17,17 @@
 
       # Prevents Grafana from phoning home
       #analytics.reporting_enabled = false;
+    };
+  };
+
+  # nginx reverse proxy
+  services.nginx.virtualHosts.${config.services.grafana.domain} = {
+    locations."/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+        proxyWebsockets = true;
+        extraConfig = ''
+          proxy_set_header Host ${config.services.grafana.domain};
+        '';
     };
   };
 }

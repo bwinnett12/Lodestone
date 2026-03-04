40832ccf4d119899a328b5236f7320f092aa7495
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-03-04 02:13:19 +0000

    Jellyfin, set generic location 3

--- resources/modules/jellyfin/default.nix
+++ resources/modules/jellyfin/default.nix
@@ -7,8 +7,8 @@
     enable = true;
     openFirewall = true;
     
-    user = "root";  # todo - Provide a better solution for this
-    #group = "jellyfin";  # todo - Provide a better solution for this
+    user = "jellyfin";  # todo - Provide a better solution for this
+    group = "jellyfin";  # todo - Provide a better solution for this
 
     #dataDir = "/var/lib/jellyfin";  # TODO  - Re-implement with 9p system
     #configDir = "/storage/Yarrow/temp5";  # TODO  - Re-implement with 9p system

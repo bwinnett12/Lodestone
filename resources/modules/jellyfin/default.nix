4d6494c00ea3358fd1161e52ba91c9b67cd2e9bc
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-03-12 08:32:21 +0000

    Jellyfin options: changed dir 2

--- resources/modules/jellyfin/default.nix
+++ resources/modules/jellyfin/default.nix
@@ -7,7 +7,7 @@
     enable = true;
     openFirewall = true;
     
-    user = "root";  # todo - Provide a better solution for this
+    user = "tarobutter";  # todo - Provide a better solution for this
     # group = "tarobutter";  # todo - Provide a better solution for this
 
     dataDir = "/storage/Yarrow/temp";  # TODO  - Re-implement with 9p system

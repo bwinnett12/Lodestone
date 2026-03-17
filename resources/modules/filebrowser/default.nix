5986c02427ffbc3444effee901e814cc9da616a8
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-03-17 00:30:41 +0000

    Filebrowser 7

--- resources/modules/filebrowser/default.nix
+++ resources/modules/filebrowser/default.nix
@@ -5,17 +5,18 @@
 
   services.filebrowser = {
     enable = true;
-    group = "users";
+
+	group = "users";
 	openFirewall = true;
 
 	user = "pomona";
-
-	extraArgs = [
-      "--port" "2000"
-      "--address" "0.0.0.0"
-      "--database" "/var/lib/filebrowser/database.db"
-    ];
-
+ 
+    settings = {
+      port = 2000;      # Using port 2000 as seen in your error
+      address = "10.0.1.10"; # Listen on your specific internal IP or "0.0.0.0"
+      database = "/var/lib/filebrowser/database.db";
+      root = "/storage/Orchid/tmep"; # Directly open to your storage
+      log = "stdout";
+    };
   };
-
 }

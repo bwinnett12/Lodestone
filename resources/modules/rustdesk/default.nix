a04419551fbd91adbe6f2ca54102748f8521b00d
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-06-04 04:49:31 +0000

    Rustdesk Server 4

--- resources/modules/rustdesk/default.nix
+++ resources/modules/rustdesk/default.nix
@@ -9,12 +9,14 @@
     
     # Force hbbs to point to this machine's IP or localhost for the relay
     signal.relayHosts = [ "127.0.0.1" ]; 
+
+    client.enable = true;
   };
 
-  environment.systemPackages = with pkgs; [
-    # ... your other packages ...
-    rustdesk-flutter
-  ];
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gnome ]; # Or -gtk / -kde depending on your DE
+  };
     
     # Optional: If you have a domain name or public IP, put it here.
     # Otherwise, it defaults to your local setup.

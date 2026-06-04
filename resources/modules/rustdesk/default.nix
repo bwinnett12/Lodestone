0677726eabae7e2fda5c0994a9d8a371f981a651
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-06-04 04:22:44 +0000

    Rustdesk Server 2

--- resources/modules/rustdesk/default.nix
+++ resources/modules/rustdesk/default.nix
@@ -7,10 +7,13 @@
     signal.enable = true;
     relay.enable = true;
     
+    # Force hbbs to point to this machine's IP or localhost for the relay
+    signal.relayHosts = [ "127.0.0.1" ]; 
+  };
+    
     # Optional: If you have a domain name or public IP, put it here.
     # Otherwise, it defaults to your local setup.
     # relay.ip = "your.public.ip.or.domain"; 
-  };
 
   # Data will automatically be stored securely in /var/lib/rustdesk-server
 }

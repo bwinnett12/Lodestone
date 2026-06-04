2b04b6e57be5bd1d71bf4f025da12ec164a28e37
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-06-04 04:18:34 +0000

    Rustdesk Server

--- resources/modules/rustdesk/default.nix
+++ resources/modules/rustdesk/default.nix
@@ -1,73 +1,16 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, ... }:
 
-let
-  rustdeskComposeFile = pkgs.writeText "rustdesk-docker-compose.yml" ''
-    version: '3.8'
-    services:
-      hbbs:
-        container_name: hbbs
-        image: rustdesk/rustdesk-server:latest
-        command: hbbs
-        volumes:
-          - /var/lib/rustdesk:/data/root
-        networks:
-          - rustdesk
-        ports:
-          - "21115:21115"
-          - "21116:21116"
-          - "21116:21116/udp"
-          - "21118:21118"
-        depends_on:
-          - hbbr
-        restart: unless-stopped
-
-      hbbr:
-        container_name: hbbr
-        image: rustdesk/rustdesk-server:latest
-        command: hbbr
-        volumes:
-          - /var/lib/rustdesk:/data/root
-        networks:
-          - rustdesk
-        ports:
-          - "21117:21117"
-          - "21119:21119"
-        restart: unless-stopped
-
-    networks:
-      rustdesk:
-  '';
-in
 {
-  virtualisation.docker.enable = true;
-
-  systemd.services.rustdesk-server = {
-    description = "RustDesk Server";
-    after = [ "docker.service" ];
-    requires = [ "docker.service" ];
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.docker-compose}/bin/docker-compose -f ${rustdeskComposeFile} up";
-      Restart = "on-failure";
-      RestartSec = 10;
-      User = "root";
-      StandardOutput = "journal";
-      StandardError = "journal";
-    };
+  # Enable RustDesk Signaling Server (hbbs)
+  services.rustdesk-server = {
+    enable = true;
+    signal.enable = true;
+    relay.enable = true;
+    
+    # Optional: If you have a domain name or public IP, put it here.
+    # Otherwise, it defaults to your local setup.
+    # relay.ip = "your.public.ip.or.domain"; 
   };
 
-  systemd.tmpfiles.rules = [
-    "d /var/lib/rustdesk 0755 root root -"
-  ];
-
-  # Dependencies - removed rustdesk
-  environment.systemPackages = with pkgs; [
-    curl
-    wget
-    unzip
-    jq
-    docker-compose
-  ];
+  # Data will automatically be stored securely in /var/lib/rustdesk-server
 }

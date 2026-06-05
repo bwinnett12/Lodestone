3b4eca4483dfdfffe7483bac0989b7bd4c985238
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-06-05 20:09:12 +0000

    Moonlight 3

--- resources/modules/moonlight/default.nix
+++ resources/modules/moonlight/default.nix
@@ -3,39 +3,21 @@
 { config, pkgs, inputs, ... }:
 
 {
+
   services.sunshine = {
     enable = true;
     autoStart = true;
-    capSysAdmin = true; # Necessary for GPU hardware capture paths
-    openFirewall = true; # Automatically opens streaming ports (47984-48010)
-
-	package = pkgs.sunshine;
+    capSysAdmin = true;   # Gives Sunshine kernel permission to capture your GPU frames
+    openFirewall = true;  # Automatically opens streaming ports (47984-48010)
+    package = pkgs.sunshine;
   };
 
-  systemd.user.services.sunshine = {
-
-	#description = "Sunshine self-hosted game stream host for Moonlight";
-    wantedBy = [ "graphical-session.target" ];
-
-    environment = {
-      XDG_SESSION_TYPE = "wayland";
-      XDG_CURRENT_DESKTOP = "GNOME";
-	  LD_LIBRARY_PATH = "/run/opengl-driver/lib:/run/opengl-driver-32/lib";
-	  LIBVA_DRIVER_NAME = "nvidia";
-
-    };
+  systemd.user.services.sunshine.unitConfig = {
+    ConditionUser = "tarobutter";
   };
 
-  security.wrappers.sunshine = {
-    owner = "root";
-    group = "root";
-    capabilities = "cap_sys_admin+ep";
-    source = "${pkgs.sunshine}/bin/sunshine";
-  };
+  hardware.uinput.enable = true;
+  #users.users.tarobutter.extraGroups = [ "uinput" "input" ];
 
-
-  systemd.user.services.sunshine.unitConfig.ConditionUser = "tarobutter";
-  environment.systemPackages = with pkgs; [
-    ];
 }
 

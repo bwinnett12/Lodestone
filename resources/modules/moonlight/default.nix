ed518e0402dbc3c7c98bb9f691845755b5930d15
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-06-05 02:45:31 +0000

    Moonlight 9

--- resources/modules/moonlight/default.nix
+++ resources/modules/moonlight/default.nix
@@ -15,16 +15,19 @@
   };
 
   systemd.user.services.sunshine = {
+
+	description = "Sunshine self-hosted game stream host for Moonlight";
+    wantedBy = [ "graphical-session.target" ];
+
     environment = {
       XDG_SESSION_TYPE = "wayland";
       XDG_CURRENT_DESKTOP = "GNOME";
 	  LD_LIBRARY_PATH = "/run/opengl-driver/lib:/run/opengl-driver-32/lib";
     };
-	
   };
 
-  systemd.user.services.sunshine.unitConfig.ConditionUser = "tarobutter";
 
+  systemd.user.services.sunshine.unitConfig.ConditionUser = "tarobutter";
   environment.systemPackages = with pkgs; [
     ];
 }

f435af6e7278fe57a345b574faf14c22dc88f7dc
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-06-05 03:27:27 +0000

    Moonlight 13

--- resources/modules/moonlight/default.nix
+++ resources/modules/moonlight/default.nix
@@ -8,9 +8,10 @@
     autoStart = true;
     capSysAdmin = true; # Necessary for GPU hardware capture paths
     openFirewall = true; # Automatically opens streaming ports (47984-48010)
-
+	
 	package = pkgs.sunshine.override {
       cudaSupport = false;
+      stdenv = pkgs.stdenv;
     };
   };
 

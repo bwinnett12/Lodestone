f0daec757f4cc1eef1fc91f332f157f4c72e7f68
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-07-10 09:12:01 +0000

    Overhaul home manager

--- resources/modules/common/default.nix
+++ resources/modules/common/default.nix
@@ -11,6 +11,8 @@
   i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = "America/Anchorage";
 
+  programs.home-manager.enable = true;
+
   ## General Nix settings
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];

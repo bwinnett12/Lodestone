594e1e53fe000ce62036bd6825511952b4e273f4
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-07-20 22:55:06 +0000

    Second copy 3

--- resources/modules/mailroom/default.nix
+++ resources/modules/mailroom/default.nix
@@ -5,7 +5,7 @@
   systemd.services.mailroom = {
     enable      = true;
     wantedBy = [ "multi-user.target" ];
-    vaultPath   = "/storage/shortstack/mailroom/vault";
+    vaultPath   = "/storage/Tulip/shortstack/mailroom/vault";
     libraryRoot = "/storage/Orchard";
     llmUrl      = "http://127.0.0.1:8090";
   };

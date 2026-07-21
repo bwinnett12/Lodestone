c2f468c0cc8164c92854b8b5ffbb6c03362dc283
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-07-21 07:36:11 +0000

    ou

--- resources/modules/mailroom/default.nix
+++ resources/modules/mailroom/default.nix
@@ -4,9 +4,8 @@
 
   systemd.services.mailroom = {
     enable      = true;
-    wantedBy = [ "multi-user.target" ];
-    vaultPath   = "/storage/Tulip/shortstack/mailroom/vault";
+    vaultPath   = "/var/lib/mailroom/vault";
     libraryRoot = "/storage/Orchard";
-    llmUrl      = "http://127.0.0.1:8090";
+    llmUrl      = "http://ai.platatoo.com/";
   };
 }

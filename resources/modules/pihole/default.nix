6b6a52dfce7304c851df12f2be2ae36ae4b0010d
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-07-26 22:17:48 +0000

    pihole option

--- resources/modules/pihole/default.nix
+++ resources/modules/pihole/default.nix
@@ -8,26 +8,26 @@
     enable = true;
 
     openFirewallDNS = true;        # so other devices on your network can query it
-      openFirewallWebserver = true;  # for the admin UI
-      
-      settings = {
-        # See <https://docs.pi-hole.net/ftldns/configfile/>
+    openFirewallWebserver = true;  # for the admin UI
+    
+    settings = {
+      # See <https://docs.pi-hole.net/ftldns/configfile/>
 
-        # External DNS Servers quad9 and cloudflare
-        dns.upstreams = [ "9.9.9.9" "1.1.1.1" ];
+      # External DNS Servers quad9 and cloudflare
+      dns.upstreams = [ "9.9.9.9" "1.1.1.1" ];
 
-        # Optionally resolve local hosts (domain is optional)
-        # dns.hosts = [ "pihole.platatoo.com" ];  ## TODO -- Is this updated for Locomotive if declared as 
-      };
+      # Optionally resolve local hosts (domain is optional)
+      # dns.hosts = [ "pihole.platatoo.com" ];  ## TODO -- Is this updated for Locomotive if declared as 
+    };
 
-    lists = [    # Lists can be added via URL
-      {
-        url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt";
-        type = "block";
-        enabled = true;
-        description = "hagezi blocklist";
-      }
-    ];
+    #lists = [    # Lists can be added via URL
+    #  {
+    #    url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt";
+    #    type = "block";
+    #    enabled = true;
+    #   description = "hagezi blocklist";
+    #  }
+    #];
   };
 
   services.pihole-web = {

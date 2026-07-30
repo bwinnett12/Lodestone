cdc6a4af6ee25fed1cf912270ba6c55039283de1
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-07-30 07:52:17 +0000

    Anki 2

--- resources/modules/anki/default.nix
+++ resources/modules/anki/default.nix
@@ -3,27 +3,30 @@
 { config, pkgs, ... }:
 
 {
-  servkces.anki-sync-server = {
-    enable = true;
-    baseDirectory = "/storage/shortstack/anki";
-    port = "3111";
-    users = "anki";
-  };
-
   # nginx reverse proxy
-  services.nginx = {
-    enable = true;
-    virtualHosts."anki.platatoo.com" = {
-      listen = [{ addr = "100.106.125.87"; port = 80; }];
-      locations."/" = {
-          proxyPass = "http://127.0.0.1:3111";
-          proxyWebsockets = true;
-          extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-          '';
+  services = {
+
+    anki-sync-server = {
+      enable = true;
+      baseDirectory = "/storage/shortstack/anki";
+      port = "3111";
+      users = "anki";
+    };
+    
+    nginx = {
+      enable = true;
+      virtualHosts."anki.platatoo.com" = {
+        listen = [{ addr = "100.106.125.87"; port = 80; }];
+        locations."/" = {
+            proxyPass = "http://127.0.0.1:3111";
+            proxyWebsockets = true;
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+            '';
+        };
       };
     };
   };

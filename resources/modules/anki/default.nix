46e9495a17b486eb8007d0646c4cef97d5a8d561
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-07-30 07:45:45 +0000

    Anki

--- resources/modules/anki/default.nix
+++ resources/modules/anki/default.nix
@@ -1,37 +1,20 @@
 #### ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ -!!- ~~~~~ ##
 #### Anki via Docker Compose
 { config, pkgs, ... }:
+
 {
-
-  systemd.services.anki-docker = {
-    description = "Anki via Docker Compose";
-    after = [ "network.target" "docker.service" ];
-    requires = [ "docker.service" ];
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-	  ExecStart = "${pkgs.docker-compose}/bin/docker-compose -f /etc/anki/docker-compose.yml up";
-	  ExecStop = "${pkgs.docker-compose}/bin/docker-compose -f /etc/anki/docker-compose.yml down";
-      ## TODO - Switch to anki or shortstack user
-      User = "root";
-      WorkingDirectory = "/storage/shortstack/anki";
-      Restart = "on-failure";
-      RestartSec = "5s";
-    };
+  servkces.anki-sync-server = {
+    enable = true;
+    baseDirectory = "/storage/shortstack/anki";
+    port = "3111";
+    users = "anki";
   };
-
-  ## Set the docker compose to /var/lib/anki/docker-compose.yml
-  environment.etc."anki/docker-compose.yml" = {
-    source = ./anki-docker.yml;
-    mode = "0644";
-  };
-  virtualisation.docker.enable = true;
 
   # nginx reverse proxy
   services.nginx = {
     enable = true;
     virtualHosts."anki.platatoo.com" = {
-      listen = [{ addr = "100.83.209.81"; port = 80; }];
+      listen = [{ addr = "100.106.125.87"; port = 80; }];
       locations."/" = {
           proxyPass = "http://127.0.0.1:3111";
           proxyWebsockets = true;
@@ -45,7 +28,7 @@
     };
   };
 
-  networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 3111 3112 3113 ];
+  networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 3111 ];
 
   ## Setup Anki user
   users = {
@@ -56,7 +39,7 @@
       createHome = true;
       home = "/var/lib/anki";
     };
-  groups.anki = {};
+    groups.anki = {};
   };
 
 }

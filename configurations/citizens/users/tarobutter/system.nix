78780c60360998dfcd495aa04ca5ad2fbe5c8db3
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-08-04 06:15:20 +0000

    Touches 2

--- configurations/citizens/users/tarobutter/system.nix
+++ configurations/citizens/users/tarobutter/system.nix
@@ -2,7 +2,7 @@
 { ... }: {
   ecosystem.users.tarobutter = {
     description = "Tarot D. Butter";
-    extraGroups = [ "networkmanager" "systemd-journal" "wheel" "docker" "input" "video" "render" "storage"];
+    extraGroups = [ "networkmanager" "systemd-journal" "wheel" "docker" "input" "video" "render" "storage" ];
     homeManagerModule = ./home.nix;
     upgraded = true;
     uid = 1500;
--- resources/citizens/upgraded.nix
+++ resources/citizens/upgraded.nix
@@ -12,7 +12,7 @@
   # Expands to only the ones that actually exist on this machine.
   # TODO - Implement an automatic solution for this
   config.ecosystem.upgradedGroups =
-    lib.optional (config.users.groups ? surface-control) "surface-control"
+       lib.optional (config.users.groups ? surface-control) "surface-control"
     ++ lib.optional (config.users.groups ? anki) "anki"
     ++ lib.optional (config.users.groups ? docker) "docker"
     ++ lib.optional (config.users.groups ? localai) "localai"
@@ -22,10 +22,7 @@
     ++ lib.optional (config.users.groups ? uinput) "uinput"
     ++ lib.optional (config.users.groups ? docker) "docker"
     ++ lib.optional (config.users.groups ? jellyfin) "jellyfin"
-
-    ++ lib.optional (config.users.groups ? storage-Orchid) "storage-Orchid"
-    ++ lib.optional (config.users.groups ? storage-Tulip) "storage-Tulip"
-    ++ lib.optional (config.users.groups ? storage-Yarrow) "storage-Yarrow"
+    ++ lib.optional (config.users.groups ? storage) "storage"
 
     ++ lib.optional (config.users.groups ? media) "media"
     ++ lib.optional (config.users.groups ? go2rtc) "go2rtc"

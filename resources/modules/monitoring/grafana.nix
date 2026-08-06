c5405bbd5ce579dd53761515689e39301a0a3ad5
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-08-06 02:14:10 +0000

    Updated Grafana

--- resources/modules/monitoring/grafana.nix
+++ resources/modules/monitoring/grafana.nix
@@ -37,8 +37,23 @@
       ];
     };
   };
+  networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 3000 ];
 
-  services.nginx.virtualHosts."grafana.platatoo.com" = {
-    locations."/".proxyPass = "http://127.0.0.1:3000";
+    # nginx reverse proxy
+  services.nginx = {
+    enable = true;
+    virtualHosts."grafana.platatoo.com" = {
+      listen = [{ addr = "100.83.209.81"; port = 80; }];
+      locations."/" = {
+          proxyPass = "http://127.0.0.1:3000";
+          proxyWebsockets = true;
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+          '';
+      };
+    };
   };
 }

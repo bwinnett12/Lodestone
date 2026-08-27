8a1dc7e30418b5a4fba772b05c8eae8981bfbeee
Bell <60406893+bwinnett12@users.noreply.github.com>
2026-08-27 01:35:25 +0000

    Mailroom tweak

--- resources/modules/mailroom/default.nix
+++ resources/modules/mailroom/default.nix
@@ -30,5 +30,19 @@
         '';
       };
     };
+
+    virtualHosts."locomotive.tail4b1127.ts.net" = {
+      listen = [{ addr = "100.106.125.87"; port = 80; }];
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8095";
+        proxyWebsockets = true;
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+    };
   };
 }
